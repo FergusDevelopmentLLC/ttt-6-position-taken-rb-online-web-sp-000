28fceb195cde37a4a56d0c61634164ccdd47bd8d
Will Carter <will.carter@fergusllc.com>
2019-12-13 20:29:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,14 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-  puts board[index]
-  return false
+  taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+  taken
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
-
 position_taken?(board, index)
