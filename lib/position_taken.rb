15eb968bf2ea0d5a9aef6325d2ac99297f86bb52
Will Carter <will.carter@fergusllc.com>
2019-12-13 20:26:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
-def position_taken (board, index)
+def position_taken? (board, index)
   puts board[index]
+  return false
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
